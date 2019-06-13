ba192267fb879f69b92480fa5364f16ad115f43f
JulianneUnyu <50496490+JulianneUnyu@users.noreply.github.com>
2019-06-12 20:48:20 -0600

    Add files via upload

--- TECPokemonRanger/src/Structures/Dijkstra.java
+++ TECPokemonRanger/src/Structures/Dijkstra.java
@@ -1,193 +1,93 @@
 package Structures;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import static GUI.PrincipalMenu.jPanel1;
+import static GUI.PrincipalMenu.R_repaint;
+import java.awt.Color;
+import javax.swing.JOptionPane;
 
-/*
- * This class allows to implement a Djistra
+/**
+ *
+ * @author fredy-18
  */
-public class Dijkstra<X> {
-    private List<Vertice<X>> vertices;
-    private List<Edge> edges;
-    private Set<Vertice<X>> controlledVertice;
-    private Set<Vertice<X>> nonControlledVertice;
-    private Map<Vertice<X>, Vertice<X>> predecessors;
-    private Map<Vertice<X>, Double> distances;
-
-    //Contructor
-    public Dijkstra(Graph graph) {
-        this.vertices = new ArrayList<Vertice<X>>(graph.getVertices());
-        this.edges = new ArrayList<Edge>(graph.getEdges());
+public class Dijkstra {
+   private  Trees trees;
+   private int subTop;
+   private Node aux=null;
+   private int accumulatedAux; 
+   private int subAccumulated;
+   private Node node[]; 
+   private int top;
+   private int permanent;     
+   private int endNode; 
+   
+   
+    public Dijkstra (Trees trees, int top, int permanent, int endNode){
+        this.trees = trees;        
+        this.top = top;
+        this.node= new Node[top]; 
+        this.permanent = permanent;
+        this.endNode = endNode;
+        
     }
 
-    //Getters and Setters
-    
-    //Return the list vertices
-    public List<Vertice<X>> getVertices() {
-        return vertices;
-	}
-
-    //Set the list of vertices
-    public void setVertices(List<Vertice<X>> vertices) {
-        this.vertices = vertices;
+    public int getAccumulated(){
+        return node[endNode].getAccumulated(); 
     }
-
-    //Return the list edges
-    public List<Edge> getEdges() {
-        return edges;
-    }
-
-    //Set the list edges
-    public void setEdges(List<Edge> edges) {
-        this.edges = edges;
-    }
-
-    //Return the controlled vertice
-    public Set<Vertice<X>> getControlledVertice() {
-        return controlledVertice;
-    }
-
-    //Set the controlled vertice
-    public void setControlledVertice(Set<Vertice<X>> controlledVertice) {
-        this.controlledVertice = controlledVertice;
-    }
-
-    //Return the non controlle vertice
-    public Set<Vertice<X>> getNonControlledVertice() {
-        return nonControlledVertice;
-    }
-
-    //Set the non controlled vertice
-    public void setNonControlledVertice(Set<Vertice<X>> nonControlledVertice) {
-        this.nonControlledVertice = nonControlledVertice;
-    }
-
-    //Return the predessors
-    public Map<Vertice<X>, Vertice<X>> getPredecessors() {
-        return predecessors;
-    }
-
-    //Set the predecessors
-    public void setPredecessors(Map<Vertice<X>, Vertice<X>> predecessors) {
-        this.predecessors = predecessors;
-    }
-
-    //Return the distances
-    public Map<Vertice<X>, Double> getDistances() {
-        return distances;
-    }
-
-    //Set the disctances
-    public void setDistances(Map<Vertice<X>, Double> distances) {
-        this.distances= distances;
-    }
-
-    //Returns true if the vertice is already counted
-    private boolean isControlled(Vertice<X> vertice) {
-        return controlledVertice.contains(vertice);
-    }
-
-    //Return of the route to the specified destination
-    private double getShorterDistance(Vertice<X> destination) {
-        Double distance = distances.get(destination);
-        if (distance == null){
-            return Double.MAX_VALUE;}
-        return distance;
-    }
-
-    //Return the shorter distance between a vertice
-    private Vertice<X> getMinimum(Set<Vertice<X>> vertices) {
-        Vertice<X> minimum = null;
-        for (Vertice<X> vertice : vertices){
-            if (minimum == null){
-                minimum = vertice;
-            }else{
-                if (getShorterDistance(vertice) < getShorterDistance(minimum))
-                    minimum = vertice;
-            }
-        }
-        return minimum;
-    }
-
-    //Return the neighbors of a vertice in a route 
-    private List<Vertice<X>> getNeighbors(Vertice<X> Vertice) {
-        List<Vertice<X>> neighbors = new ArrayList<Vertice<X>>();
-        for (Edge edge : edges){
-            if (edge.getStartingPoint().equals(Vertice) && !isControlled(edge.getArrivalPoint())){
-                neighbors.add(edge.getArrivalPoint());
-            }
-        }
-        return neighbors;
-    }
-
-    //Return the distance between two vertices
-    private double getDistance(Vertice<X> starting, Vertice<X> arrival) {
-            for (Edge edge : edges) {
-                if (edge.getStartingPoint().equals(starting) && edge.getArrivalPoint().equals(arrival)){
-                    return edge.getLength();
-                }
-            }
-            throw new RuntimeException("This vertices does not have union with each other");
-    }
-
-    //Return the minimun distance between two vertices
-    private void getMinimunDistance(Vertice<X> Vertice) {
-        List<Vertice<X>> neighborVertices = getNeighbors(Vertice);
-        for (Vertice<X> destinationVertice : neighborVertices) {
-            if (getShorterDistance(destinationVertice) > getShorterDistance(Vertice) + getDistance(Vertice, destinationVertice)){
-                    distances.put(destinationVertice,getShorterDistance(Vertice) + getDistance(Vertice, destinationVertice));
-                    predecessors.put(destinationVertice, Vertice);
-                    nonControlledVertice.add(destinationVertice);
-            }
-        }
-    }
-
-    /**
-     * This method allows to generate a general mapping from a vertice.
-     * This method must be run before getting GetListPath()
-     * @param Vertice
-     */
-    public void executeGraph(Vertice<X> Vertice) {
-        controlledVertice = new HashSet<>();
-        nonControlledVertice = new HashSet<>();
-        distances = new HashMap<>();
-        predecessors = new HashMap<>();
-        distances.put(Vertice, 0.0);
-        nonControlledVertice.add(Vertice);
-
-        while(nonControlledVertice.size() > 0) {
-            Vertice<X> vertice = getMinimum(nonControlledVertice);
-            controlledVertice.add(vertice);
-            nonControlledVertice.remove(vertice);
-            getMinimunDistance(vertice);
-        }
-    }
-
-    /**
-     * It allows to generate a list of nodes through which it is directed to the destination by a shorter distance
-     * This method must be executed after executingGraph ();
-     * 
-     * @param Destino
-     * @return 
-     */
-    public LinkedList<Vertice<X>> getListPath(Vertice<X> Destino) {
-            LinkedList<Vertice<X>> DijkstraPath = new LinkedList<>();
-            Vertice<X> stroke = Destino;
-            if (predecessors.get(stroke) == null){
-                    return null;
-            }
-            DijkstraPath.add(stroke);
-            while (predecessors.get(stroke) != null) {
-                    stroke = predecessors.get(stroke);
-                    DijkstraPath.add(stroke);
-            }                
-            Collections.reverse(DijkstraPath);
-            return DijkstraPath;
+        
+    public void dijkstra(){ 
+         for (int i = 0; i < top; i++)
+            node[i]= new Node(); 
+         
+        if(permanent != endNode){
+             jPanel1.paint(jPanel1.getGraphics());
+             R_repaint(top, trees);   
+             Paint.clickOnNode(jPanel1.getGraphics(), trees.getXcoordenate(permanent), trees.getYcoordenate(permanent), null,Color.GREEN); 
+            
+        
+            node[permanent].setVisitated(true);        
+            node[permanent].setName(permanent);       
+            
+            do{            
+              subAccumulated=0;
+              accumulatedAux = 2000000000; // lo igualamos a esta cifra ya q el acomulado de los nodos, supuestamente  nunca sera mayor 
+              node[permanent].setLabel(true); 
+              for (int j = 0; j < top; j++) {
+                  if(trees.getMadjacency(j, permanent)==1){
+                        subAccumulated= node[permanent].getAccumulated()+trees.getMcoefficient(j, permanent);                                 
+                        if(subAccumulated <= node[j].getAccumulated()&& node[j].isVisitated()==true && node[j].isLabel()== false){
+                            node[j].setAccumulated(subAccumulated);
+                            node[j].setVisitated(true);
+                            node[j].setName(j);
+                            node[j].setPredecessor(node[permanent]);
+                        }
+                        else if( node[j].isVisitated()==false){
+                            node[j].setAccumulated(subAccumulated);
+                            node[j].setVisitated(true);
+                            node[j].setName(j);
+                            node[j].setPredecessor(node[permanent]); 
+                       }
+                 }
+              }
+              for (int i = 0; i <top; i++) { // buscamos cual de los nodos visitado tiene el acomulado menor par escogerlo como permanente 
+                if(node[i].isVisitated()== true && node[i].isLabel()== false){
+                   if(node[i].getAccumulated()<=accumulatedAux){
+                       permanent= node[i].getName();
+                       accumulatedAux= node[i].getAccumulated();
+                   }
+                }               
+             }
+            subTop++;                
+          }while(subTop<top+1);          
+           aux= node[endNode]; 
+           if(aux.getPredecessor() == null )
+             JOptionPane.showMessageDialog(null,"Could not reach the node "+endNode);          
+          while(aux.getPredecessor() != null){           
+              Paint.paintPath(jPanel1.getGraphics(), trees.getXcoordenate(aux.getName()), trees.getYcoordenate(aux.getName()), trees.getXcoordenate(aux.getPredecessor().getName()), trees.getYcoordenate(aux.getPredecessor().getName()),Color.GREEN);
+              Paint.clickOnNode(jPanel1.getGraphics(), trees.getXcoordenate(aux.getName()), trees.getYcoordenate(aux.getName()), null,Color.GREEN);
+             aux=aux.getPredecessor();              
+          }  
+         Paint.clickOnNode(jPanel1.getGraphics(), trees.getXcoordenate(endNode), trees.getYcoordenate(endNode), null,Color.GREEN);     
+       }
+       else Paint.clickOnNode(jPanel1.getGraphics(), trees.getXcoordenate(endNode), trees.getYcoordenate(endNode), null,Color.GREEN);    
     }
 }
